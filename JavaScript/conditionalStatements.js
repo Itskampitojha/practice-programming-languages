9cd3dd94b309b5e0921f056127701e7882f89011
kampit ojha <97059622+Itskampitojha@users.noreply.github.com>
2024-08-23 01:17:15 +0530

    conditional statements

---
+++ JavaScript/conditionalStatements.js
@@ -0,0 +1,24 @@
+// If-else statement
+
+let score = 85;
+if (score >= 90) {
+    console.log("Grade A");
+} else if (score >= 80) {
+    console.log("Grade: B")
+} else{
+    console.log("Grade: C");
+}
+
+// Switch statement
+
+let fruit = "apple";
+switch(fruit) {
+    case "apple" :
+        console.log("It's an appple");
+        break;
+    case "banana":
+        console.log("It's a banana")
+        break;
+    default:
+        console.log("Unknown fruit")
+}
