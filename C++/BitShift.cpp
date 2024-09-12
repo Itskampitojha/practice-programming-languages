d98048539696283737aea54532204ae5d9b06944
kampit ojha <97059622+Itskampitojha@users.noreply.github.com>
2024-09-12 09:01:06 +0530

    practice

---
+++ C++/BitShift.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = 5;
+
+    int leftShift = num << 1;
+    cout << "left Shift: " << leftShift << endl;
+
+    int rightShift = num >> 1;
+    cout <<"rightshift: " << rightShift << endl;
+
+    return 0;
+}
