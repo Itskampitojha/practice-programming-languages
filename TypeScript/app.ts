00f06e30b5777659081ef87b03b3e29a3078389b
kampit ojha <97059622+Itskampitojha@users.noreply.github.com>
2024-09-13 20:07:05 +0530

    ts basics

---
+++ TypeScript/app.ts
@@ -0,0 +1,41 @@
+// p o l i c e  = character
+
+
+//primitive and reference
+ a = 12
+
+ b = a
+
+
+ b+2
+
+
+
+ a = []
+
+
+//  if bracket then it is reference otherwise it will be primitive
+
+
+var a = true;
+
+
+
+// you cant copy references but copy primitives
+
+
+let a = 12;
+
+a = "harsh"
+
+let arr = [];
+
+// lets create enums
+
+//tuples
+
+[1,2,3,4,5,6 ]
+
+
+
+
