734bf4cd73fd77a9306f585a8cda904af027308d
kampit ojha <97059622+Itskampitojha@users.noreply.github.com>
2024-09-28 09:47:43 +0530

    Practice Routes

---
+++ React JS/practice/src/useparams.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const useparams = () => {
+  return (
+    <div>
+      {
+        parth:/ student .: id,
+        element
+      }
+    </div>
+  )
+}
+
+export default useparams
+
+
+
+
+
+
+useNavigagtion
+
+<button>
+    <div>
+        Home oage <buttnon on click ={()=>navigate('/')}    
+        
+    </div>
+
